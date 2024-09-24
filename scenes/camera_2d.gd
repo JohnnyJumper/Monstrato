2ce5080406935f38dc0d7452e27c9207b988f8b0
Johnny Tahirov <jeyhunt@gmail.com>
2024-09-23 23:53:54 -0700

    advanced zoom in and zoom out

--- scenes/camera_2d.gd
+++ scenes/camera_2d.gd
@@ -1,21 +1,34 @@
 extends Camera2D
 
-var ZOOM_MIN = Vector2(0.5, 0.5)
-var ZOOM_MAX = Vector2(5.0, 5.0)
+var ZOOM_MIN: Vector2
+var ZOOM_MAX: Vector2
+var ZOOM_TARGET: Vector2
+var ZOOM_SPEED: float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	ZOOM_MIN = Vector2(0.5, 0.5)
+	ZOOM_MAX = Vector2(5.0, 5.0)
+	ZOOM_SPEED = 10
+	ZOOM_TARGET = zoom
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	handleZoom(delta)
+
+
+
+func handleMovement() -> void:
+	var mousePos = get_global_mouse_position()
+	#if Input.is_action_just_pressed("MouseMiddleClick"):
+				
+
+
+func handleZoom(delta: float) -> void: 
 	if Input.is_action_just_pressed("ZoomIn"):
-		zoom *= 1.1
+		ZOOM_TARGET *= 1.1
 	if Input.is_action_just_pressed("ZoomOut"):
-		zoom /= 1.1
-	
-	zoom = clamp(zoom, ZOOM_MIN, ZOOM_MAX)
-
-
-
-	
+		ZOOM_TARGET *= 0.9
+	ZOOM_TARGET = clamp(ZOOM_TARGET, ZOOM_MIN, ZOOM_MAX)
+	zoom = zoom.lerp(ZOOM_TARGET, ZOOM_SPEED * delta)
